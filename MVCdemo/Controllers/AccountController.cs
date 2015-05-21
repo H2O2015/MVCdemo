6c684524c1e0698b4c7c0e0874bab8f348b82631
H2O2015 <2821864154@qq.com>
2015-05-21 23:06:56 +0800

    20150521
    第一天的学习

---
+++ MVCdemo/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCdemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.LoginState = "登录前...";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            // 获取表单数据
+            string email = fc["inputEmail3"];
+            string password = fc["inputPassword3"];
+
+            // 进行下一步处理
+            ViewBag.LoginState = "登录后...";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
